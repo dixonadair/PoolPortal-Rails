0e0f84e9801ba67c76d9459a7ec495ab4e95e20b
Dixon Adair <dixonadairv@gmail.com>
2015-06-22 00:26:56 -0700

    fixed hs and hps time and distance calcs

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'json'
 gem 'foreigner'
 gem 'gon' # allows you to pass variables from controller directly to js file
 gem 'paperclip', '~> 3.5.3'
+gem 'nokogiri'
 
 group :production do
 	gem 'pg'
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,9 @@ require 'httparty'
 require 'json'
 require 'gon'
 
+require 'nokogiri'
+require 'open-uri'
+
 # -----------------------------------------
 
 class MainController < ApplicationController
@@ -15,8 +18,10 @@ class MainController < ApplicationController
     end
     gon.school_coords = [-84.378611, 33.833333]
     gon.current_user = current_user
-
     gon.all_options = Family.where.not(id: session[:user_id])
+    page = Nokogiri::HTML(open("http://www.atlantagasprices.com/index.aspx"))
+    cur_price = page.css('.SideAvg tr:eq(2) td:eq(2)').text
+    @cur_price = cur_price.to_f
   end
 
   def one_pool
@@ -28,53 +33,55 @@ class MainController < ApplicationController
     pool_coords = []
     pool_coords << pool_family.lng << pool_family.lat
 
-    # -------------------------
+    # ------------------------- Google Maps (Home-to-School) -------------------------
 
-    # Google Maps
-    hs_url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{home_coords[1]},#{home_coords[0]}&destination=#{school_coords[1]},#{school_coords[0]}&key=AIzaSyDirDB7V1F3KwSFublV4KmZPhOGnJ71BLE"
-    hs_response = HTTParty.get(hs_url).to_json
-    hs_response = JSON.parse(hs_response)
+      # hs_url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{home_coords[1]},#{home_coords[0]}&destination=#{school_coords[1]},#{school_coords[0]}&key=AIzaSyDirDB7V1F3KwSFublV4KmZPhOGnJ71BLE"
+      # hs_response = HTTParty.get(hs_url).to_json
+      # hs_response = JSON.parse(hs_response)
 
-    hs_time = hs_response["routes"][0]["legs"][0]["duration"]["value"]
-    hs_distance = hs_response["routes"][0]["legs"][0]["distance"]["value"].meters_to_miles
+      # hs_time = hs_response["routes"][0]["legs"][0]["duration"]["value"]
+      # hs_distance = hs_response["routes"][0]["legs"][0]["distance"]["value"].meters_to_miles
 
-    # family = Family.find(session[:user_id])
-    # hs_time = family.hs_time
-    # hs_distance = family.hs_distance
+    family = Family.find(session[:user_id])
+    hs_time = family.hs_time
+    hs_distance = family.hs_distance
 
-    # -------------------------
+    # ------------------------- Google Maps (Home-to-Pool-to-School) -------------------------
 
-    # Google Maps
-    hps_url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{home_coords[1]},#{home_coords[0]}&destination=#{school_coords[1]},#{school_coords[0]}&waypoints=#{pool_coords[1]},#{pool_coords[0]}&key=AIzaSyDirDB7V1F3KwSFublV4KmZPhOGnJ71BLE"
-    hps_response = HTTParty.get(hps_url).to_json
-    hps_response = JSON.parse(hps_response)
+      # hps_url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{home_coords[1]},#{home_coords[0]}&destination=#{school_coords[1]},#{school_coords[0]}&waypoints=#{pool_coords[1]},#{pool_coords[0]}&key=AIzaSyDirDB7V1F3KwSFublV4KmZPhOGnJ71BLE"
+      # hps_response = HTTParty.get(hps_url).to_json
+      # hps_response = JSON.parse(hps_response)
 
-    hps_time = (hps_response["routes"][0]["legs"][0]["duration"]["value"] + hps_response["routes"][0]["legs"][1]["duration"]["value"])
-    hps_distance = (hps_response["routes"][0]["legs"][0]["distance"]["value"] + hps_response["routes"][0]["legs"][1]["distance"]["value"]).meters_to_miles
+      # hps_time = (hps_response["routes"][0]["legs"][0]["duration"]["value"] + hps_response["routes"][0]["legs"][1]["duration"]["value"])
+      # hps_distance = (hps_response["routes"][0]["legs"][0]["distance"]["value"] + hps_response["routes"][0]["legs"][1]["distance"]["value"]).meters_to_miles
 
-    # -------------------------
+    hp_url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{home_coords[1]},#{home_coords[0]}&destination=#{pool_coords[1]},#{pool_coords[0]}&key=AIzaSyDirDB7V1F3KwSFublV4KmZPhOGnJ71BLE"
+    hp_response = HTTParty.get(hp_url).to_json
+    hp_response = JSON.parse(hp_response)
 
-      # response = { hi: "hi", hs_time: hs_time, hs_distance: hs_distance, hps_time: hps_time, hps_distance: hps_distance, pool_family: pool_family }
+    hp_time = hp_response["routes"][0]["legs"][0]["duration"]["value"]
+    hp_distance = hp_response["routes"][0]["legs"][0]["distance"]["value"].meters_to_miles
 
-      # response = { pool_partial: render_to_string(file, layout: false), hi: "hi" }
+    hps_time = hp_time + pool_family.hs_time
+    hps_distance = hp_distance + pool_family.hs_distance
 
-      # response = { pool_partial: render_to_string('main/_pool_option', layout: false) }
+    # -------------- Get current price of gas for Atlanta area from gasbuddy --------------
 
-    # --------- Non-graph version ----------
+    page = Nokogiri::HTML(open("http://www.atlantagasprices.com/index.aspx"))
+    cur_price = page.css('.SideAvg tr:eq(2) td:eq(2)').text
+    cur_price = cur_price.to_f
+
+    # --------- Text-only version ----------
 
       # response = { pool_partial: render_to_string('main/_pool_option', layout: false, locals: { pool_family: pool_family, hs_time: hs_time, hs_distance: hs_distance, hps_time: hps_time, hps_distance: hps_distance }) }
 
     # --------- C3 Graph version ----------
 
-    # To use with 'main/_pool_option_2'
-    # response = { pool_partial: render_to_string('main/_pool_option_2', layout: false, locals: { pool_family: pool_family }), hs_gas: ((hs_distance*10)/25).round(2), hs_emissions: (((hs_distance*10)/25)*19.64).round(2), hs_dollars: (((hs_distance*10)/25)*2.70).round(2), hs_time: (((hs_time*10).round(2))/3600).round(2), hs_distance: (hs_distance*10).round(2), hps_gas: ((hps_distance*5)/25).round(2), hps_emissions: (((hps_distance*5)/25)*19.64).round(2), hps_dollars: (((hps_distance*5)/25)*2.70).round(2), hps_time: (((hps_time*5).round(2))/3600).round(2), hps_distance: (hps_distance*5).round(2) }
+      # response = { pool_partial: render_to_string('main/_pool_option_2', layout: false, locals: { pool_family: pool_family }), hs_gas: ((hs_distance*10)/25).round(2), hs_emissions: (((hs_distance*10)/25)*19.64).round(2), hs_dollars: (((hs_distance*10)/25)*2.70).round(2), hs_time: (((hs_time*10).round(2))/3600).round(2), hs_distance: (hs_distance*10).round(2), hps_gas: ((hps_distance*5)/25).round(2), hps_emissions: (((hps_distance*5)/25)*19.64).round(2), hps_dollars: (((hps_distance*5)/25)*2.70).round(2), hps_time: (((hps_time*5).round(2))/3600).round(2), hps_distance: (hps_distance*5).round(2) }
 
-    # To use with 'main/_pool_option_3'
-    response = { pool_partial: render_to_string('main/_pool_option_3', layout: false, locals: { pool_family: pool_family, hs_gas: ((hs_distance*10)/25).round(2), hs_emissions: (((hs_distance*10)/25)*19.64).round(2), hs_dollars: (((hs_distance*10)/25)*2.70).round(2), hs_time: hs_time*10, hs_distance: (hs_distance*10).round(2), hps_gas: ((hps_distance*5)/25).round(2), hps_emissions: (((hps_distance*5)/25)*19.64).round(2), hps_dollars: (((hps_distance*5)/25)*2.70).round(2), hps_time: hps_time*5, hps_distance: (hps_distance*5).round(2) }) }
+    # --------- Image + Savings version ----------
 
-    # response = { pool_partial: render_to_string('main/_pool_option_3', layout: false, locals: { pool_family: pool_family }) }
-
-    # response = { wtf: render_to_string('main/test', layout: false) }
+    response = { pool_partial: render_to_string('main/_pool_option_3', layout: false, locals: { pool_family: pool_family, hs_gas: ((hs_distance*10)/25).round(2), hs_emissions: (((hs_distance*10)/25)*19.64).round(2), hs_dollars: (((hs_distance*10)/25)*cur_price).round(2), hs_time: hs_time*10, hs_distance: (hs_distance*10).round(2), hps_gas: ((hps_distance*5)/25).round(2), hps_emissions: (((hps_distance*5)/25)*19.64).round(2), hps_dollars: (((hps_distance*5)/25)*cur_price).round(2), hps_time: hps_time*5, hps_distance: (hps_distance*5).round(2), cur_price: cur_price }) }
 
   	render json: response
   end
