d1bb0051cb8939be228db64222a08fbf750be0ec
Dixon Adair <dixonadairv@gmail.com>
2015-06-30 13:29:51 -0700

    stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,35 +5,38 @@ set :application, 'PoolPortal_Rails' #UNCOMMENT
 set :repo_url, 'https://github.com/dixonadair/PoolPortal-Rails' #UNCOMMENT
 # set :repo_url, 'git@example.com:me/my_repo.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+# ==================================
 
-# Default value for :scm is :git
-# set :scm, :git
+	# Default branch is :master
+	# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default value for :format is :pretty
-# set :format, :pretty
+	# Default deploy_to directory is /var/www/my_app_name
+	# set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+	# Default value for :scm is :git
+	# set :scm, :git
 
-# Default value for :pty is false
-# set :pty, true
+	# Default value for :format is :pretty
+	# set :format, :pretty
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+	# Default value for :log_level is :debug
+	# set :log_level, :debug
 
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+	# Default value for :pty is false
+	# set :pty, true
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+	# Default value for :linked_files is []
+	# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+	# Default value for linked_dirs is []
+	# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+	# Default value for default_env is {}
+	# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+	# Default value for keep_releases is 5
+	# set :keep_releases, 5
 
 # ==================================
 
@@ -53,6 +56,10 @@ set :repo_url, 'https://github.com/dixonadair/PoolPortal-Rails' #UNCOMMENT
 # ==================================
 
 #UNCOMMENT BELOW
+
+role :app, "your app-server here"
+role :web, "your web server here"
+role :db, "your db-server here", :primary => true
 
 after "deploy:symlink", "deploy:update_crontab"
 
